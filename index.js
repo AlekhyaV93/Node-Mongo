73d0f8e0d91b7129a6be376303550d987f40e08a
Alekhya <vinnakotaalekhya@gmail.com>
2020-08-14 00:05:53 -0500

    Refactored the code to use promises instead of callbacks to avoid callback hell

--- index.js
+++ index.js
@@ -1,52 +1,58 @@
 const MongoClient = require('mongodb').MongoClient; //importing MongoClient object of the mongodb node driver
-const assert = require('assert').strict; //importing the core module assert
 
 const dbOperations = require('./operations');
 
 const url = 'mongodb://localhost:27017/'; //connection string to connect with the local mongodb
 const dbname = 'nucampsite'; //database name to which we would do data manipulations
 
-//connecting to the mongodb server through mongoclient, by passing in a connection string and a callback function
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-
-    assert.strictEqual(err, null); //verifying if there are any errors
+//connecting to the mongodb server through mongoclient, by passing in a connection string and handling the promise
+MongoClient.connect(url, { useUnifiedTopology: true }).then(client => {
 
     console.log('Connected to the server'); //else console.log message
 
     const db = client.db(dbname); // accessing the nucampsite database
 
     //dropping the collection campsites from nucampsite db
-    db.dropCollection('campsites', (err, result) => {
+    db.dropCollection('campsites')
+        .then(result => {
 
-        assert.strictEqual(err, null);
+            console.log('Dropped Collection', result); //the dropcollection method returns a boolean value, console.log the result
+        })
+        .catch(err => console.log('No collection to drop.'));
 
-        console.log('Dropped Collection', result); //the dropcollection method returns a boolean value, console.log the result
-
-        //inserting one document into the collection 
-        dbOperations.insertDocument(db, { name: "Breadcrumb trail campground", description: "Test" }, 'campsites', result => {
+    //inserting one document into the collection    
+    dbOperations.insertDocument(db, { name: "Breadcrumb trail campground", description: "Test" }, 'campsites')
+        .then(result => {
 
             console.log('Insert Document', result.ops);//console.log the inserted document
             //finding the document
-            dbOperations.findDocument(db, 'campsites', docs => {
-                console.log("Found Documents", docs);
-                //updating the document
-                dbOperations.updateDocument(db, { name: "Breadcrumb trail campground" }, { description: "Updated the description" }, 'campsites', result => {
-                    console.log("Updated document", result.result.nModified);
+            return dbOperations.findDocument(db, 'campsites');
+        })
+        .then(docs => {
+            console.log("Found Documents", docs);
+            //updating the document
+            return dbOperations.updateDocument(db, { name: "Breadcrumb trail campground" }, { description: "Updated the description" }, 'campsites')
+        })
+        .then(result => {
+            console.log("Updated document", result.result.nModified);
 
-                    dbOperations.findDocument(db, 'campsites', docs => {
-                        console.log("Found Updated Documents", docs)
+            return dbOperations.findDocument(db, 'campsites')
+        })
+        .then(docs => {
+            console.log("Found Updated Documents", docs)
 
-                        //removing the document
-                        dbOperations.removeDocument(db, { name: "Breadcrumb trail campground" }, 'campsites', result => {
-                            console.log('Deleted Document Count:', result.deletedCount);
+            //removing the document
+            return dbOperations.removeDocument(db, { name: "Breadcrumb trail campground" }, 'campsites')
+        })
+        .then(result => {
+            console.log('Deleted Document Count:', result.deletedCount);
 
-                            client.close();//closing the connection
-                        });
-                    })
-
-                });
-            });
-
-        });
-    });
-});
+            return client.close();//closing the connection
+        }).catch(err => {
+            console.log(err);
+            client.close();
+        })
+})
+    .catch(err => {
+        console.log(err)
+    })
